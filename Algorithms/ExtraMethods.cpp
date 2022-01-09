82521a8f84f5f18f516b3c4d52bf561ebcece430
Happy <poddarsayan22@gmail.com>
2022-01-09 13:35:39 +0530

    add reverse func

--- Algorithms/ExtraMethods.cpp
+++ Algorithms/ExtraMethods.cpp
@@ -2,16 +2,37 @@
 #include <algorithm>
 using namespace std;
 
-int main(){
+int main()
+{
 
-    int a=10, b=20;
-    cout << "Before Swap" << endl << a << " and " << b << endl;
-    swap(a,b);
-    cout << "After Swap" << endl << a << " and " << b << endl;
-    
+    int a = 10, b = 20;
+    cout << "Before Swap" << endl
+         << a << " and " << b << endl;
+    swap(a, b);
+    cout << "After Swap" << endl
+         << a << " and " << b << endl;
+
     // max and min func
-    cout << max(a,b) << endl;
-    cout << min(a,b) << endl;
-    
-    
+    cout << max(a, b) << endl;
+    cout << min(a, b) << endl;
+
+    // add reverse func
+    int arr[] = {1, 2, 3, 4, 5};
+    int n = sizeof(arr) / sizeof(arr[0]);
+
+    cout << "Original array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
+
+    reverse(arr, arr + 4);
+
+    cout << "Reverse array" << endl;
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
 }
