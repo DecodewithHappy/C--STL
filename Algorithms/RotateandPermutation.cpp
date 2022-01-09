ec946f59cef255776647a8ef7801b4899649a447
Happy <poddarsayan22@gmail.com>
2022-01-09 12:51:45 +0530

    Add vector

--- Algorithms/RotateandPermutation.cpp
+++ Algorithms/RotateandPermutation.cpp
@@ -8,10 +8,20 @@ int main(){
     int arr[] = {10,20,30,40,50};
     int n = sizeof(arr)/sizeof(arr[0]);
 
-    rotate(arr, arr+1, arr+n);
+    rotate(arr, arr+1, arr+n); //rotate the array w.r.t to arr+1(2nd element)
 
     for(int i=0; i< n; i++){
         cout << arr[i] << " ";
     }
     cout << endl;
+
+    //using vector
+    vector<int> v{1,2,3,7,9,10};
+    
+    rotate(v.begin(), v.begin()+3, v.end());
+ 
+    for(int i = 0; i < v.size(); i++){
+        cout << v[i] << " ";
+    }
+
 }
