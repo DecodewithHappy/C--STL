0d5e8c84b0f356112d0614c961b541be2e3a6e8a
Happy <poddarsayan22@gmail.com>
2022-01-09 11:52:01 +0530

    Initiate Coin change

---
+++ Algorithms/MoneyChange.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+int main(){
+
+    //Coin change problem
+    int coins[] = {1,2,5,10,20,50,100,200,500,2000};
+    int money = 168; 
+    //we need to check values which is close to the money
+    
+}
