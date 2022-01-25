e50aae9682367c64885c4009c493183d0186fe09
Happy <poddarsayan22@gmail.com>
2022-01-26 01:28:21 +0530

    Add Queue using stack

--- Stack&Queue/StackusingQueue.cpp
+++ Stack&Queue/StackusingQueue.cpp
@@ -4,74 +4,87 @@ using namespace std;
 
 // Implement a Stack using 2 Queues
 
-template<typename T>
-class Stack{
-    queue<T> q1,q2;
+template <typename T>
+class Stack
+{
+    queue<T> q1, q2;
+
 public:
-    void push(T x){
+    void push(T x)
+    {
         q1.push(x);
     }
-    void pop(){
-        //remove the last added element from q1
-        //we have to move first n-1 elements in q2
-        //interchange the names of q1 and q2
-        if(q1.empty()){
+    void pop()
+    {
+        // remove the last added element from q1
+        // we have to move first n-1 elements in q2
+        // interchange the names of q1 and q2
+        if (q1.empty())
+        {
             return;
         }
-        while(q1.size()>1){
+        while (q1.size() > 1)
+        {
             T element = q1.front();
             q2.push(element);
             q1.pop();
         }
-        //removes the last element 
+        // removes the last element
         q1.pop();
-        //swap the names of q1 and q2
-        swap(q1,q2);
+        // swap the names of q1 and q2
+        swap(q1, q2);
     }
-    T top(){
-
-        while(q1.size()>1){
-            T element = q1.front();
+    T top()
+    {
+        T element;
+        while (q1.size() >= 1)
+        {
+            element = q1.front();
             q2.push(element);
             q1.pop();
         }
 
-        //1 element in Q1
-        T element = q1.front();
-        q1.pop();
-        q2.push(element);
+        // 1 element in Q1
+        //  T element = q1.front();
+        //  q1.pop();
+        //  q2.push(element);
 
-        swap(q1,q2);
+        swap(q1, q2);
         return element;
     }
-    int size(){
+    int size()
+    {
         return q1.size() + q2.size();
     }
-    bool empty(){
-        return size()==0;
+    bool empty()
+    {
+        return size() == 0;
     }
-
 };
 
-int main() {
+int main()
+{
 
     Stack<int> s;
     s.push(1);
     s.push(2);
     s.push(3);
-    
-    while(!s.empty()){
-        cout<<s.top()<<" ";
+
+    while (!s.empty())
+    {
+        cout << s.top() << " ";
         s.pop();
     }
+    cout << endl;
 
     s.push(4);
     s.push(5);
 
-    while(!s.empty()){
-        cout<<s.top()<<" ";
+    while (!s.empty())
+    {
+        cout << s.top() << " ";
         s.pop();
     }
-    
+
     return 0;
 }
