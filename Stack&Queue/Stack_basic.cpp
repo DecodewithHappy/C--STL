6c90e52e9fb65fbd94e307adc7c98e1d0b567181
Happy <poddarsayan22@gmail.com>
2022-01-25 21:08:05 +0530

    init stack

---
+++ Stack&Queue/Stack_basic.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include<stack>
+using namespace std;
+
+
+int main() {
+
+    stack<int> s;
+    int n;
+    cout << "Enter the number of elements in the stack : ";
+    cin >> n;
+    cout << endl;
+
+    for(int i=0; i<n; i++){
+        s.push(i);
+    }
+
+    //Loop
+    while(!s.empty()){
+        cout<<s.top()<<" ,";
+        s.pop();
+    }
+
+    return 0;
+}
